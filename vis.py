35c6b05ca2336596c1a77b2d8827da8d38d34865
RyuatS <sitomi0216@icloud.com>
2019-05-20 15:10:34 +0900

    refatoring

--- vis.py
+++ vis.py
@@ -13,21 +13,26 @@ from tensorflow.python.framework import graph_util
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
-from lib.core.config import BACKBONE_NAME_LIST, _KEYPOINTS_LABEL, CONNECT
-
+from lib.core.config import BACKBONE_NAME_LIST, _KEYPOINTS_LABEL
 
 # user packages
 from lib.models.hourglass import Hourglass
+import lib.utils.helper as helper
+
 
 FLAGS = tf.app.flags.FLAGS
-
 tf.app.flags.DEFINE_string('check_dir',
                            './checkpoints_dir',
                            'checkpoints directory')
 
 tf.app.flags.DEFINE_string('image_path',
-                           './god.jpg',
-                           '')
+                           './data/demo/god.jpg',
+                           'image you want to predict keypoints')
+
+tf.app.flags.DEFINE_boolean('is_separate',
+                           False,
+                           'whether you visualize heatmaps separately or not.')
+
 
 def main(argv):
 
@@ -57,75 +62,9 @@ def main(argv):
         res = res[0]
         res = cv2.resize(res, (192, 256))
 
+        helper.visualize_heatmaps(load_image, predict=res, is_separate=FLAGS.is_separate)
 
-        # load_image = load_image.astype(np.float32)
-        # gray = cv2.cvtColor(load_image, cv2.COLOR_RGB2GRAY)
-        # gray /= 255
-        # res = np.max(res, axis=2)
-        # result = (gray + res) / 2
-        # plt.imshow(result, cmap='gray')
-        # plt.title('result')
-        # plt.show()
-
-        # plt.figure(figsize=(12, 10))
-        # plt.subplot(3, 6, 1)
-        # plt.imshow(load_image.astype(np.uint8))
-        # num_channel = res.shape[2]
-        # for i in range(num_channel):
-        #     plt.subplot(3, 6, i+2)
-        #     plt.imshow(res[:,:,i], cmap='gray')
-        #     plt.title(_KEYPOINTS_LABEL[i])
-        # plt.show()
-
-        keypoint_locs = []
-        num_channel = res.shape[2]
-        for i in range(num_channel):
-            h = res[:,:, i]
-            temp = h.ravel()
-            temp = sorted(temp)
-            max_10 = temp[-20]
-            h[h < max_10] = 0
-            h[h >= max_10] = 1
-            plt.imshow(h)
-            plt.title(_KEYPOINTS_LABEL[i])
-            plt.show()
-
-            width_sum = np.sum(h, axis=0).flatten()
-            height_sum = np.sum(h, axis=1).flatten()
-            mean_x = 0
-            mean_y = 0
-            count_x = 0
-            count_y = 0
-            for x in range(len(width_sum)):
-                if width_sum[x] >= 1:
-                    mean_x += x
-                    count_x += 1
-            for y in range(len(height_sum)):
-                if height_sum[y] >= 1:
-                    mean_y += y
-                    count_y += 1
-
-            key_x = mean_x / count_x
-            key_y = mean_y / count_y
-            keypoint_loc = [key_x, key_y]
-            keypoint_locs.append(keypoint_loc)
-
-        for CON in CONNECT:
-            ind1, ind2 = CON
-            key1 = keypoint_locs[ind1]
-            key2 = keypoint_locs[ind2]
-            key1 = np.array(key1).astype(np.int)
-            key2 = np.array(key2).astype(np.int)
-            load_image = cv2.line(load_image, tuple(key1),tuple(key2),(255,0,0),1)
-
-        for i in range(len(keypoint_locs)):
-            x, y = keypoint_locs[i]
-            plt.scatter(x, y, label=_KEYPOINTS_LABEL[i])
-        plt.imshow(load_image.astype(np.uint8))
-        plt.legend()
-        plt.show()
-
-
+        helper.visualize_keypoints(load_image, predict_heatmap=res)
 
     else:
         raise ValueError("'{}' does not exist".format(FLAGS.check_dir))
