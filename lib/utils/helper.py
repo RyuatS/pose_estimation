576cd2cc563c419514205c78980c4559001e229c
RyuatS <sitomi0216@icloud.com>
2019-05-20 15:09:52 +0900

    add function for visualize heatmaps and keypoints

--- lib/utils/helper.py
+++ lib/utils/helper.py
@@ -22,6 +22,8 @@ import tensorflow as tf
 from tensorflow.contrib.distributions import MultivariateNormalDiag
 import sys
 import os
+import cv2
+from lib.core.config import _KEYPOINTS_LABEL, SKELETON
 
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 from core.config import R_MEAN, G_MEAN, B_MEAN
@@ -103,3 +105,118 @@ def create_heatmap(img, keypoint, sigma=1.0):
 
     result = result.reshape((img.shape[0], img.shape[1]))
     return result
+
+
+def visualize_heatmaps(image, target=None, predict=None, is_separate=False):
+    """
+    visualize input image, target heatmaps and heatmaps.
+
+    Args:
+        image       : input image. [height, width, channel].
+        target      : target heatmaps. [height, width, the number of keypoints]. The number of keypoints should be 17.
+        predict    : predict heatmaps. [height, widht, the number of keypoints]. The number of keypoints hould be 17.
+        is_separate : whether heatmaps separate or not.
+    """
+
+    plot_rows, plot_cols = 3, 6
+    plt.figure(figsize=(12, 10))
+    if target is not None:
+        num_keys = target.shape[2]
+        if is_separate:
+            plt.subplot(plot_rows, plot_cols, 1)
+            plt.imshow(image)
+            plt.title('input image')
+            image = image.astype(np.float32)
+            for index in range(num_keys):
+                plt.subplot(plot_rows, plot_cols, index+2)
+                plt.imshow(target[:, :, index], cmap='gray')
+                plt.title(_KEYPOINTS_LABEL[index])
+        else:
+            image = image.astype(np.float32)
+            gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            if np.max(gray) > 1:
+                gray = gray / 255
+            _2d_heatmap = np.max(target, axis=2)
+            gray = (gray + _2d_heatmap) / 2
+
+            plt.imshow(gray, cmap='gray')
+        plt.show()
+
+    if predict is not None:
+        num_keys = predict.shape[2]
+        if is_separate:
+            plt.subplot(plot_rows, plot_cols, 1)
+            plt.imshow(image)
+            plt.title('input image')
+            image = image.astype(np.float32)
+            for index in range(num_keys):
+                plt.subplot(plot_rows, plot_cols, index+2)
+                plt.imshow(predict[:, :, index], cmap='gray')
+                plt.title(_KEYPOINTS_LABEL[index])
+        else:
+            image = image.astype(np.float32)
+            gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            if np.max(gray) > 1:
+                gray /= 255
+            _2d_heatmap = np.max(predict, axis=2)
+            gray  = (gray + _2d_heatmap) / 2
+
+            plt.imshow(gray, cmap='gray')
+        plt.show()
+
+    if (predict is None) and (target is None):
+        print('There is nothing to plot')
+
+
+def visualize_keypoints(image, predict_heatmap=None):
+    """
+    visualize keypoints using line.
+
+    Args:
+        image: input image. [height, width, 3]
+        predict_heatmap : heatmaps which is outputed from model.
+    """
+
+    keypoint_loc_list = []
+    num_channel = predict_heatmap.shape[2]
+
+    for i in range(num_channel):
+        h = predict_heatmap[:, :, i]
+        h_flatten = sorted(h.ravel())
+
+        threshold = h_flatten[-20]
+        h[h < threshold] = 0
+        h[h > threshold] = 1
+
+        width_sum = np.sum(h, axis=0).flatten()
+        height_sum = np.sum(h, axis=1).flatten()
+        loc_x = 0
+        loc_y = 0
+        count_x = 0
+        count_y = 0
+        for x in range(len(width_sum)):
+            if width_sum[x] >= 1:
+                loc_x += x
+                count_x += 1
+
+        for y in range(len(height_sum)):
+            if height_sum[y] >= 1:
+                loc_y += y
+                count_y += 1
+
+        key_x = loc_x // count_x
+        key_y = loc_y // count_y
+        keypoint_loc_list.append([key_x, key_y])
+
+    for CON in SKELETON:
+        index1, index2 = CON
+        key1 = keypoint_loc_list[index1]
+        key2 = keypoint_loc_list[index2]
+        cv2.line(image, tuple(key1), tuple(key2), (255, 0, 0), 2)
+
+    for i in range(len(keypoint_loc_list)):
+        x, y = keypoint_loc_list[i]
+        plt.scatter(x, y, label=_KEYPOINTS_LABEL[i])
+    plt.imshow(image.astype(np.uint8))
+    plt.legend()
+    plt.show()
